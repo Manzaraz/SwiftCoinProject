99da4b9576968f7d301f746b14fedbb71c3df595
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-08-29 22:59:08 -0300

    Initial Commit

---
+++ SwiftCoinProject/SwiftCoinProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinProjectApp.swift
+//  SwiftCoinProject
+//
+//  Created by Christian Manzaraz on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
