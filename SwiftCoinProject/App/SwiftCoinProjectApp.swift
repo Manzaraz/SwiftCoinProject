cff1e30a99d70f01aeef33e898686fb40885a309
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-08-29 23:19:21 -0300

    Project Setup: 	AppStuff

--- SwiftCoinProject/App/SwiftCoinProjectApp.swift
+++ SwiftCoinProject/App/SwiftCoinProjectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftCoinProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ SwiftCoinProject/Core/Home/Views/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  SwiftCoinProject
+//
+//  Created by Christian Manzaraz on 29/08/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                // Top movers View
+                
+                // All movers View
+                
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
